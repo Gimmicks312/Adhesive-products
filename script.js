6351d4c26de0bbe1447b90dce0a64cd43cd0159d
Gimmicks312 <edingermaurice@hotmail.de>
2025-03-08 16:05:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,20 +21,20 @@ fetch('https://raw.githubusercontent.com/Gimmicks312/Adhesive-products/main/prod
 // Function to display products in the table
 function displayProducts(products) {
     const tableBody = document.getElementById('productTableBody');
-    tableBody.innerHTML = '';
+    tableBody.innerHTML = '';  // Clear the table before adding new rows
 
     products.forEach(product => {
         let row = document.createElement('tr');
 
         let fields = [
-            'id', 'name', 'category', 'basis', 'softeningPoint', 'viscosity.30', 'viscosity.120', 
-            'viscosity.140', 'viscosity.160', 'viscosity.180', 'viscosity.200', 'density', 
-            'color', 'solidContent', 'ph', 'applicationTemperature', 'feedingSpeed'
+            'id', 'name', 'category', 'basis', 'appearance', 'color', 'softeningPoint', 'viscosity.30', 'viscosity.120',
+            'viscosity.140', 'viscosity.160', 'viscosity.180', 'viscosity.200', 'density', 'solidContent', 'ph',
+            'applicationTemperature', 'feedingSpeed', 'applicationQuantity', 'openTime'
         ];
 
         fields.forEach(field => {
             let cell = document.createElement('td');
-            let value = getNestedValue(product, field) || '';
+            let value = getNestedValue(product, field) || '';  // Get value, default to empty string if undefined
             cell.textContent = value;
             row.appendChild(cell);
         });
